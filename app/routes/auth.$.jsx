5bc9c767a2df35e10dc6ee7f42ab2ae8a14c08d7
Sourav Patil <souravpatil1711@gmail.com>
2026-04-17 15:54:59 +0530

    update app

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -1,11 +1,22 @@
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { authenticate } from "../shopify.server";
 
-export const loader = async ({ request }) => {
-  await authenticate.admin(request);
+// export const loader = async ({ request }) => {
+//   await authenticate.admin(request);
 
-  return null;
+//   return null;
+// };
+export const loader = async ({ request }) => {
+
+  const { session } = await authenticate.admin(request);
+
+  const shop = session.shop;
+
+  return redirect(
+    `https://app.bolka.ai/login?shop=${shop}&source=shopify`
+  );
 };
+
 
 export const headers = (headersArgs) => {
   return boundary.headers(headersArgs);
