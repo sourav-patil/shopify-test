52f26fced96b1c9c068b7dc6c0304f06fe5de274
Sourav Patil <souravpatil1711@gmail.com>
2026-04-20 11:17:11 +0530

    fix auth redirect loop

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -1,22 +1,10 @@
-import { boundary} from "@shopify/shopify-app-react-router/server";
+import { boundary } from "@shopify/shopify-app-react-router/server";
 import { authenticate } from "../shopify.server";
-import { redirect } from "react-router";
-// export const loader = async ({ request }) => {
-//   await authenticate.admin(request);
 
-//   return null;
-// };
 export const loader = async ({ request }) => {
-
-  const { session } = await authenticate.admin(request);
-
-  const shop = session.shop;
-
-  return redirect(
-    `https://app.bolka.ai/login?shop=${shop}&source=shopify`
-  );
+  await authenticate.admin(request);
+  return null;
 };
-
 
 export const headers = (headersArgs) => {
   return boundary.headers(headersArgs);
