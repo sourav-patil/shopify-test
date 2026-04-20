9ab9c13bc205418a256973063030856dc7107e41
Sourav Patil <souravpatil1711@gmail.com>
2026-04-20 13:03:52 +0530

    fix auth redirect loop

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -16,6 +16,7 @@ const shopify = shopifyApp({
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
+  isEmbeddedApp: false,  
   future: {
     expiringOfflineAccessTokens: true,
   },
