5c6e6714d150957e4dda22ae59d37ec4aba12de3
Sourav Patil <souravpatil1711@gmail.com>
2026-04-21 13:00:25 +0530

    fix auth redirect loop

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -27,7 +27,6 @@ const shopify = shopifyApp({
 
 export default shopify;
 export const apiVersion = ApiVersion.October25;
-export const addDocumentResponseHeaders = shopify.addDocumentResponseHeaders;
 export const authenticate = shopify.authenticate;
 export const unauthenticated = shopify.unauthenticated;
 export const login = shopify.login;
