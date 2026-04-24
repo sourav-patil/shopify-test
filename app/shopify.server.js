4bcfca4bfb931fb8de80e9e780aa5cf3ac38dadd
Sourav Patil <souravpatil1711@gmail.com>
2026-04-24 10:25:43 +0530

    change to non embbed app different method

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -5,12 +5,16 @@ import prisma from "./db.server";
 export const shopify = shopifyApi({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET,
+
   scopes: process.env.SCOPES.split(","),
-  hostName: process.env.HOST.replace(/^https?:\/\//, ""),
+
+  hostName: process.env.SHOPIFY_APP_URL.replace(/^https?:\/\//, ""),
+
   apiVersion: "2024-10",
+
   isEmbeddedApp: false,
+
   sessionStorage: new PrismaSessionStorage(prisma),
 });
 
-// ADD THIS LINE
 export const authenticate = shopify.authenticate;
