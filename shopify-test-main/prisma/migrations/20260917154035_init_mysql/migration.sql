861ed32789ad9734092656dd011b88f26d582219
Sourav Patil <souravpatil1711@gmail.com>
2026-09-17 21:48:18 +0530

    changing db to mysql

---
+++ shopify-test-main/prisma/migrations/20260917154035_init_mysql/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE `Session` (
+    `id` VARCHAR(191) NOT NULL,
+    `shop` VARCHAR(191) NOT NULL,
+    `state` VARCHAR(191) NOT NULL,
+    `isOnline` BOOLEAN NOT NULL DEFAULT false,
+    `scope` VARCHAR(191) NULL,
+    `expires` DATETIME(3) NULL,
+    `accessToken` VARCHAR(191) NOT NULL,
+    `refreshToken` VARCHAR(191) NULL,
+    `refreshTokenExpires` DATETIME(3) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ShopifySubscription` (
+    `shop` VARCHAR(191) NOT NULL,
+    `plan` VARCHAR(191) NOT NULL,
+    `active` BOOLEAN NOT NULL DEFAULT false,
+    `subscriptionId` VARCHAR(191) NULL,
+    `status` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`shop`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
